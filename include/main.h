dc2b14bb6ea7be60374b89c691a81295f375bf7c
ianseeingbeing <igillette2006@gmail.com>
2023-12-27 17:42:54 -0500

    reorganized project, added multi input funcs

--- include/main.h
+++ include/main.h
@@ -35,7 +35,6 @@
 #define PROS_USE_LITERALS
 
 #include "api.h"
-// #include "func.hpp"
 
 /**
  * You should add more #includes here
--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,16 @@ const int LAUNCH_SPEED = 120;
 const int LAUNCH_LIFT_SPEED = 44;
 const int INTAKE_SPEED = 80;
 
-int buttonLauncher;
-int buttonLauncherUp;
-int buttonLauncherDown;
+int update_launcher_input();
+int update_launcher_lift_input();
+int update_intake_input();
+int update_flap_input();
 
-int buttonIntakeIn;
-int buttonIntakeOut;
-int buttonIntakeOff;
-
-int buttonPistonOut;
-int buttonPistonIn;
+void tank_drive(int, int);
+void launcher(int, int);
+void launcher_lift(int, int);
+void intake(int, int);
+void flaps(int);
 
 // controller
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
@@ -24,11 +24,21 @@ pros::Motor motor_left_b(9, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER
 pros::Motor motor_left_c(8, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER_DEGREES);
 
 // right drivetrain
-pros::Motor motor_right_a(20, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor motor_right_b(19, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor motor_right_c(18, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor motor_right_a(1, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor motor_right_b(2, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor motor_right_c(3, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER_DEGREES);
 
-void tank_drive(int, int);
+// launcher
+pros::Motor motor_launch(5, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor motor_launch_lift(6, pros::E_MOTOR_GEAR_GREEN, true, pros::E_MOTOR_ENCODER_DEGREES);
+
+// intake
+pros::Motor motor_intake(20, pros::E_MOTOR_GEAR_GREEN, true, pros::E_MOTOR_ENCODER_DEGREES);
+
+// pistons
+pros::ADIDigitalOut piston_flap_a(1);
+// pros::ADIDigitalOut piston_flap_b(1);
+
 
 /*
  * Runs initialization code. This occurs as soon as the program is started.
@@ -94,29 +104,54 @@ void opcontrol() {
 
 	while (true) {
 
-		// update_inputs();
-		tank_drive(controller.get_analog(ANALOG_LEFT_Y), controller.get_analog(ANALOG_RIGHT_Y));
-		// launcher(LAUNCH_SPEED, 1);
-		// intake(INTAKE_SPEED, 1);
-		// flaps(1);
+		tank_drive(controller.get_analog(ANALOG_LEFT_Y), controller.get_analog(ANALOG_RIGHT_Y));	
+		launcher(LAUNCH_SPEED, update_launcher_input());
+		launcher_lift(LAUNCH_LIFT_SPEED, update_launcher_lift_input());
+		intake(INTAKE_SPEED, update_intake_input());
+		flaps(update_flap_input());
 	
-		pros::delay(20);
 	}
 }
 
-// Updates all the inputs for the processes
-// void update_inputs() {
-// 	buttonLauncher = controller.get_digital(DIGITAL_A);
-// 	buttonLauncherUp = controller.get_digital(DIGITAL_Y);
-// 	buttonLauncherDown = controller.get_digital(DIGITAL_B);
+int update_launcher_input() {
+	if (controller.get_digital(DIGITAL_A)) {
+		return 1; // on
+	}
+	return 0; // off
+}
 
-// 	buttonIntakeIn = controller.get_digital(DIGITAL_R1);
-// 	buttonIntakeOut = controller.get_digital(DIGITAL_R2);
-// 	buttonIntakeOff = controller.get_digital(DIGITAL_L1);
+int update_launcher_lift_input() {
+	if (controller.get_digital(DIGITAL_Y)) {
+		return 1; // up
+	}
+	if (controller.get_digital(DIGITAL_B)) {
+		return 2; // down
+	}
+	return 0; // off
+}
 
-// 	buttonPistonOut = controller.get_digital(DIGITAL_UP);
-// 	buttonPistonIn = controller.get_digital(DIGITAL_DOWN);
-// }
+int update_intake_input() {
+	if (controller.get_digital(DIGITAL_R1)) {
+		return 1; // consume
+	} 
+	else if (controller.get_digital(DIGITAL_R2)) {
+		return 2; // eject
+	}
+	else if (controller.get_digital(DIGITAL_L1)) {
+		return 0; // off
+	}
+	return -1; // error
+}
+
+int update_flap_input() {
+	if (controller.get_digital(DIGITAL_UP)) {
+		return 1; // up
+	}
+	if (controller.get_digital(DIGITAL_DOWN)) {
+		return 2; // down
+	}
+	return 0; // off
+}
 
 void tank_drive(int leftInput, int rightInput) {
 
@@ -125,6 +160,58 @@ void tank_drive(int leftInput, int rightInput) {
     motor_left_c = -leftInput;
 
 	motor_right_a = -rightInput;
-	motor_right_b = rightInput;
+	motor_right_b = -rightInput;
 	motor_right_c = rightInput;
+}
+
+// Launch Motor : type --> HOLD
+void launcher(int speed, int state) {
+	if (state) {
+		motor_launch = speed;
+		pros::delay(10);
+	}
+	else {
+		motor_launch = 0;
+		pros::delay(10);
+	}
+}
+
+// Launcher Lift Motor : type --> TOGGLE
+void launcher_lift(int speed, int state) {
+	if (state == 1) {
+		motor_launch_lift = speed;
+	}
+	if (state == 2) {
+		motor_launch_lift = -speed;
+	}
+	motor_launch_lift = 0;
+	pros::delay(10);
+}
+
+// Intake Motor : type --> TOGGLE
+void intake(int speed, int state) {
+	if (state == 1) {
+		motor_intake = speed;
+		pros::delay(10);
+	}
+	if (state == 2) {
+		motor_intake = -speed;
+		pros::delay(10);
+	}
+	if (state == 0) {
+		motor_intake = 0;
+		pros::delay(10);
+	}
+}
+
+// Flap Pistons : type --> TOGGLE
+void flaps(int state) {
+	if (state) {
+		piston_flap_a.set_value(HIGH);
+		pros::delay(100);
+	}
+	if (state) {
+		piston_flap_a.set_value(LOW);
+		pros::delay(100);
+	}
 }

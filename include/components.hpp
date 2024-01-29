e3ce360fa25ea0e83d928cba50faebe6e463c5a4
ianseeingbeing <igillette2006@gmail.com>
2024-01-28 18:45:17 -0800

    added back all code, fixed issue not building

---
+++ include/components.hpp
@@ -0,0 +1,24 @@
+#include "main.h"
+
+extern const std::vector<int> leftChassisMotors;
+extern const std::vector<int> rightChassisMotors;
+
+// extern std::vector<int> leftChassisMotorsReversed;
+// extern std::vector<int> rightChassisMotorsReversed;
+
+// chassis
+extern Drive chassis;
+
+// launcher
+extern pros::Motor motor_launch;
+extern pros::Motor motor_launch_lift;
+
+// intake
+extern pros::Motor motor_intake;
+
+// pistons
+extern pros::ADIDigitalOut piston_flap_a;
+extern pros::ADIDigitalOut piston_flap_b;
+
+// potentiometer
+extern pros::ADIAnalogIn auton_switch;
